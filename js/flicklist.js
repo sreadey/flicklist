ac19032adcc334ef36f75aa12748b07dc05fbeb9
Steve Readey <sreadey@yahoo.com>
2017-03-21 09:17:04 -0400

    Finish Flicklist Studio 0

--- js/flicklist.js
+++ js/flicklist.js
@@ -2,9 +2,9 @@
 
 var api = {
 	root: "https://api.themoviedb.org/3",
-	token: "" // TODO put your api key here
+	token: "3856666bbcd0424b3579af479d1ee845" // TODO put your api key here
 }
-
+// v4 auth: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzODU2NjY2YmJjZDA0MjRiMzU3OWFmNDc5ZDFlZTg0NSIsInN1YiI6IjU4ZDEyMjBkYzNhMzY4MzhmOTAwYTY5YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.P6P34El8PU411Qd45_tUYxwRIR4HhcwQOw4ruyNLCec
 /**
  * Makes an AJAX request to themoviedb.org, asking for some movies
  * if successful, prints the results to the console
